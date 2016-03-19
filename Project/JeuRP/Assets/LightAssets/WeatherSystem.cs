87ccfb643698d9b1af69739d365fcc8157fa3ccc
SimonJeSuisTonBro <simon.royer007@gmail.com>
2016-03-19 13:39:01 +0100

    Added trees, weather system, modified ground textures.

---
+++ Project/JeuRP/Assets/LightAssets/WeatherSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WeatherSystem : MonoBehaviour 
+{
+    public int DayNightCycleLength;
+    public Light lightSource;
+    public Transform ParticleSystem, player;
+
+    void Update()
+    {
+
+        // SUNLIGHT MNG
+
+        transform.Rotate(360 * Time.deltaTime / DayNightCycleLength, 0, 0);
+        if (transform.rotation.eulerAngles.y == 0)
+        {
+            if (transform.rotation.eulerAngles.x <= 335)
+                lightSource.intensity = 1f;
+        }
+        else // <-- transform.rotation.eulerAngles.y == 180
+        {
+            if (transform.rotation.eulerAngles.x >= 335)
+                lightSource.intensity = 0f;
+        }
+
+        ParticleSystem.position = player.position;
+        ParticleSystem.Translate(0, 0, -20);
+    }
+}
